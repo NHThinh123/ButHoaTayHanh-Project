17c4e9232bbb4229e015cdfc4a8a68c5f0bb1ab5
Thinh <155166003+NHThinh123@users.noreply.github.com>
2024-07-31 16:21:47 +0700

    install-morgan

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
